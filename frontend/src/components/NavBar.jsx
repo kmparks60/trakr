9f2dd657218e42498d3df00e79df0681ceb3b1ad
Ken <kmparks60@gmail.com>
2025-04-14 10:07:00 -0400

    New test branch created.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -17,7 +17,7 @@ function NavBar() {
 
 	return (
 		<nav className="bg-[#001F3F] text-white shadow-md py-4 px-8 flex justify-between items-center border-b-4 border-[#FFE8F0]">
-			<div className="flex space-x-6">
+			{/* <div className="flex space-x-6">
 				<button 
 					className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
 					onClick={() => navigate('/')}
@@ -30,43 +30,44 @@ function NavBar() {
 				>
 					Leaderboard
 				</button>
-			</div>
+			</div> */}
 
 			<div className="flex-grow text-center">
 				<Link to="/" className="text-2xl font-semibold text-white">
 					<img 
-					src="https://64.media.tumblr.com/0148460d14734cf35d3d9f6cc9877e12/1786fc6644ea9e29-68/s540x810/a16a0bc3f979c04df9e3315b259e52632588d5d8.pnj" 
-					alt="Trakr Logo" 
-					className="md:h-18 mb-4 mx-auto" 
+						src="https://64.media.tumblr.com/0148460d14734cf35d3d9f6cc9877e12/1786fc6644ea9e29-68/s540x810/a16a0bc3f979c04df9e3315b259e52632588d5d8.pnj" 
+						alt="Trakr Logo" 
+						className="md:h-18 mb-4 mx-auto" 
 					/>
 				</Link>
 			</div>
 
-			<div className="space-x-4 flex items-center">
+			{/* Fixed width with a more defined flex setup */}
+			{/* <div className="flex items-center space-x-4 min-w-[200px] justify-end">
 				{!token ? (
 					<>
-					<button 
-						className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
-						onClick={() => navigate('/register')}
-					>
-						Sign Up
-					</button>
-					<button 
-						className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
-						onClick={() => navigate('/login')}
-					>
-						Login
-					</button>
+						<button 
+							className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
+							onClick={() => navigate('/register')}
+						>
+							Sign Up
+						</button>
+						<button 
+							className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
+							onClick={() => navigate('/login')}
+						>
+							Login
+						</button>
 					</>
 				) : (
 					<button 
-					onClick={handleLogout} 
-					className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
+						onClick={handleLogout} 
+						className="bg-[#FF8532] text-[#FFFFFF] py-2 px-6 rounded-md text-sm font-sans uppercase tracking-wider hover:bg-[#FFE8F0] hover:text-[#001F3F] focus:outline-none transform hover:scale-105 transition-all duration-300"
 					>
-					Logout
+						Logout
 					</button>
 				)}
-			</div>
+			</div> */}
 		</nav>
 	);
 }

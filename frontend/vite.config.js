9789c5d8bb001bbf180f533617c82d7a311d2a73
Ken Parks <120283088+kmparks60@users.noreply.github.com>
2025-07-17 15:02:10 -0400

    Revert "Removing vercel.json"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../backend/frontend/dist',  
+  },
+  base: '/',
 });
